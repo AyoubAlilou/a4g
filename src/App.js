86168264ea1c2af5f10343119e69005a225f60d8
EL RUUUL3R ♛ <90483727+AyoubAlilou@users.noreply.github.com>
2023-12-21 19:38:48 +0100

    oooo

--- src/App.js
+++ src/App.js
@@ -3,40 +3,6 @@ function App() {
     <>
       <h1 className='title'>c4a.dev</h1>
       <h1>hello</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>Ghassen</h1>
-      <h1>ya3tek 3asba</h1>
     </>
   )
 }
